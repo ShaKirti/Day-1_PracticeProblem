a16ac955fc2e80cde54debb6f3380b7a65b637fc
Shashank <mishra.shashankofficial06@gmail.com>
2022-12-21 13:44:55 +0530

    Added: Prigram for printing default values of primitive data types

--- src/com/day1/Assignment.java
+++ src/com/day1/Assignment.java
@@ -1,7 +1,33 @@
 package com.day1;
 
+class defaultValuesOfPrimitiveDataTypes{
+    int i;
+    double d;
+    float f;
+    long l;
+    boolean istrue;
+    byte b;
+    short s;
+    char ch;
+
+    void printValues(){
+        System.out.println("Default value of int: "+i);
+        System.out.println("Default value of double: "+d);
+        System.out.println("Default value of float: "+f);
+        System.out.println("Default value of long: "+l);
+        System.out.println("Default value of boolean: "+istrue);
+        System.out.println("Default value of byte: "+b);
+        System.out.println("Default value of short: "+s);
+        System.out.println("Default value of char: "+ch);
+    }
+}
 public class Assignment {
     public static void main(String[] args) {
         System.out.println("Welcome to Day 1 practice problem.");
+
+        // Default value of all primitive datatypes
+
+        defaultValuesOfPrimitiveDataTypes myObj = new defaultValuesOfPrimitiveDataTypes();
+        myObj.printValues();
     }
 }
