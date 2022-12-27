3f60456f10263f1a1339048974bfdddbff685ba5
Shashank <mishra.shashankofficial06@gmail.com>
2022-12-28 01:32:12 +0530

    Added: Program to check string equality

--- src/com/day1/Assignment.java
+++ src/com/day1/Assignment.java
@@ -1,5 +1,7 @@
 package com.day1;
 
+import java.util.Scanner;
+
 class defaultValuesOfPrimitiveDataTypes{
     int i;
     double d;
@@ -21,13 +23,32 @@ class defaultValuesOfPrimitiveDataTypes{
         System.out.println("Default value of char: "+ch);
     }
 }
+
+class StringEquality{
+    void checkStringEquality() {
+        System.out.println("Enter the first string:");
+        String str1 = new Scanner(System.in).nextLine();
+
+        System.out.println("Enter the second string:");
+        String str2 = new Scanner(System.in).nextLine();
+
+        if (str1.equals(str2)){
+            System.out.println("Strings are equal.");
+        }
+        else
+            System.out.println("String are not equal.");
+    }
+}
 public class Assignment {
     public static void main(String[] args) {
         System.out.println("Welcome to Day 1 practice problem.");
 
         // Default value of all primitive datatypes
-
         defaultValuesOfPrimitiveDataTypes myObj = new defaultValuesOfPrimitiveDataTypes();
         myObj.printValues();
+
+        // Check the string equality
+        StringEquality stringEquality = new StringEquality();
+        stringEquality.checkStringEquality();
     }
 }
